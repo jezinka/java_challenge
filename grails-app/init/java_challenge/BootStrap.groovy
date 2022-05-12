33a51bad8af278e252644dbbbc10f6bfd9818741
jezinka <jezina1989@gmail.com>
2022-05-12 08:44:04 +0200

    prepare temp directory

--- grails-app/init/java_challenge/BootStrap.groovy
+++ grails-app/init/java_challenge/BootStrap.groovy
@@ -1,8 +1,15 @@
 package java_challenge
 
+import static java_challenge.FileService.TEMP_DIRECTORY
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        if (!new File(TEMP_DIRECTORY).exists()) {
+            new File(TEMP_DIRECTORY).mkdir()
+        }
+
     }
     def destroy = {
     }
